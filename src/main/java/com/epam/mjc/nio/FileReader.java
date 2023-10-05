067266115c2a3128785038283fddc767737e1a14
fenix <fenixtheidragon@gmail.com>
2023-10-05 14:56:47 +0300

    The same

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,24 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
-
-
+import java.io.*;
+import java.util.logging.Logger;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Logger log = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+        Profile profile = null;
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+            StringBuilder textSB = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                textSB.append(line).append(" ");
+            }
+            String text = textSB.toString();
+            String[] parts = text.split(" ");
+            profile = new Profile(parts[1], Integer.valueOf(parts[3]), parts[5], Long.valueOf(parts[7]));
+        } catch (IOException e) {
+            log.info(e.toString());
+        }
+        return profile;
     }
 }
